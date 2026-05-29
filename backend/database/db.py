c6b77cf495c0d28d122eadb2a3046945e364abc9
Pranjal <pranjalnagre29@gmail.com>
2026-05-29 16:36:51 +0530

    setup done successfully

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,7 +1,12 @@
 import sqlite3
+import os
 
 def get_db_connection():
 
-    conn = sqlite3.connect("database/fabricbill.db")
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    db_path = os.path.join(BASE_DIR, "fabricbill.db")
+
+    conn = sqlite3.connect(db_path)
 
     return conn
